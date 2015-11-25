ad3e1000a108adab38dd04a51ce10c816cd579f1
wuwood <kilokingw@gmail.com>
2015-11-26 00:10:38 +0800

    ssh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,13 +51,13 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server '128.199.163.183',
+   user: 'root',
+   roles: %w{web app},
+   ssh_options: {
+     user: 'root', # overrides user setting above
+     keys: %w(~/.ssh/authorized_keys),
+     forward_agent: false,
+     auth_methods: %w(publickey password)
+   # password: 'please use keys'
+  }
